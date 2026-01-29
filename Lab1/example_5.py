357bfd740b12a3cab5e8b73e7b2e2722c4bc6a62
Dias Bekbolatuly <dirrayoo@gmail.com>
2026-01-29 10:17:15 +0500

    Delete Lab1/example_5.py

--- Lab1/example_5.py
+++
@@ -1,7 +0,0 @@
-student = {
-        "Name": "Dias",
-        "Course": 1,
-        "Age": 18
-        }
-
-print(f"Student - {student["Name"]}\nCourse - {student["Course"]}\nAge - {student["Age"]}")
